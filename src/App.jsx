bb02a4c861a1d48383c7371e496b6b21ea459881
yunus-emre-yaman <yunusemreyaman359@gmail.com>
2025-07-09 16:39:18 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,19 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import c2wIcon from "/c2w_icon.png";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a
-          href="https://github.com/Code2Work/pizza"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={c2wIcon} className="logo" alt="Code2Work logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Code2Work + 🍕</h1>
+    <div className="app">
+      <h1>🍕 Pizza Sipariş Uygulaması</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Pizza sayısı {count}
+        <button onClick={() => setCount((prev) => prev + 1)}>
+          Pizza sayısı: {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p>Butona tıklayarak pizza sayısını artırabilirsiniz.</p>
       </div>
-      <p className="read-the-docs">Click on the Code2Work logo to learn more</p>
-    </>
+    </div>
   );
 }
 
